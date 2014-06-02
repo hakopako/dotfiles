ac85f236df9e94d2f189419d784652de871fc6e0
hakopako <hakohakohako.0518@gmail.com>
2014-06-02 18:49:52 +0900

    setup.shにオプションつけた

--- setup.sh
+++ setup.sh
@@ -2,11 +2,48 @@
 
 DIRPATH=$(cd $(dirname $0); pwd)
 
-for FILE in ${DIRPATH}/\.[a-z]*
+while getopts drh OPT
 do
-    if [ ${FILE##*/} != ".git" ]; then
-        ln -sf ${FILE} ${HOME}/${FILE##*/}
-        echo "Success: ${FILE##*/} -> ${HOME}/${FILE##*/}"
-    fi
+	case $OPT in
+		"d" ) FLAG_D="TRUE" ;;
+		"r" ) FLAG_R="TRUE" ;;
+		"h" ) FLAG_H="TRUE" ;;
+	esac
 done
+
+if [ "$FLAG_D" = "TRUE" ]; then
+	for FILE in ${DIRPATH}/\.[a-z]*
+	do
+		if [ ${FILE##*/} != ".git" ]; then
+			echo "${FILE##*/} -> ${HOME}/${FILE##*/}"
+		fi
+	done
+	exit 0
+fi
+
+if [ "$FLAG_R" = "TRUE" ]; then
+	for FILE in ${DIRPATH}/\.[a-z]*
+	do
+		if [ ${FILE##*/} != ".git" ]; then
+			ln -sf ${FILE} ${HOME}/${FILE##*/}
+			echo "Success: ${FILE##*/} -> ${HOME}/${FILE##*/}"
+		fi
+	done
+	exit 0
+fi
+
+if [ "$FLAG_H" = "TRUE" ]; then
+	echo "--[ USAGE ]-------------"
+	echo "  'sh setup.sh [OPTION]' "
+	echo ""
+	echo "--[ OPTIONS ]-----------"
+	echo "  -d , dry-run"
+	echo "  -r , run"
+	echo "  -h , help"
+	echo ""
+	exit 0
+fi
+
+echo "unknown command. try option '-h' to see usage."
+
 
